7393b6fc8980b441ec31707cb64f3dcf9a027fed
Joe Hale <jmartyhale@gmail.com>
2015-06-28 11:28:00 -0500

    Add Attributes solutions

--- crowdfund.rb
+++ crowdfund.rb
@@ -1,9 +1,15 @@
 class Project
+  attr_reader :funding, :funding_goal
+  attr_accessor :name
 
   def initialize(name, funding=0, funding_goal=1000)
-    @name = name
+    @name = name.capitalize
     @funding = funding
     @funding_goal = funding_goal
+  end
+
+  def funds_needed
+    @funding_goal - @funding
   end
 
   def to_s
@@ -25,26 +31,49 @@ class Project
 
 end
 
-
+# Instantiate 4 projects
 project1 = Project.new("LMN", 0, 2000)
 project2 = Project.new("XYZ", 0, 3000)
 project3 = Project.new("ALT", 50, 1000)
 project4 = Project.new("NOO", 0, 75)
 
+# Print out the project's details
 puts project1
 puts project2
 puts project3
 puts project4
 puts "\n"
 
+# Add/remove funds to the each project
 project1.add_funds(100)
 project2.add_funds(300)
-project3.remove_funds(100)
+project3.remove_funds(50)
 project4.add_funds(200)
 puts "\n"
 
+# Print out the project's details again
 puts project1
 puts project2
 puts project3
 puts project4
+puts "\n"
+
+# Print the funding still needed for each project
+puts project1.funds_needed
+puts project2.funds_needed
+puts project3.funds_needed
+puts project4.funds_needed
+puts "\n"
+
+# Change a project's name and print it
+project3.name = "ROI"
+project3.add_funds(250)
+puts project3
+puts "\n"
+
+# Print the funding still needed for each project again
+puts project1.funds_needed
+puts project2.funds_needed
+puts project3.funds_needed
+puts project4.funds_needed
 puts "\n"
--- studio_game.rb
+++ studio_game.rb
@@ -1,12 +1,18 @@
 class Player
+ attr_accessor :name
+ attr_reader :health
 
  def initialize(name, health=100)
    @name = name.capitalize
    @health = health
  end
 
+ def name=(new_name)
+   @name = new_name.capitalize
+ end
+
  def to_s
-   "I'm #{@name} and my health is #{@health}."
+   "I'm #{@name} and my health is #{@health} and my score is #{score}."
  end
 
  def blam
@@ -19,28 +25,48 @@ class Player
    puts "#{@name} got w00ted!"
  end
 
+ def score
+   @health + @name.length
+ end
+
 end
 
+# Instantiate 4 players
 player1 = Player.new("larry", 90)
 player2 = Player.new("curly", 120)
 player3 = Player.new("moe", 75)
 player4 = Player.new("shemp")
 
-
+# Print out player's details
 puts player1
 puts player2
 puts player3
 puts player4
 puts "\n"
 
+# Blam or W00t players
 player1.w00t
 player2.blam
 player3.w00t
 player4.w00t
 puts "\n"
 
+# Print out player's new details
 puts player1
 puts player2
 puts player3
 puts player4
 puts "\n"
+
+# Print player's anonymous health
+puts player1.health
+puts player2.health
+puts player3.health
+puts player4.health
+puts "\n"
+
+# Change Larry's name to Lawrence, then print it.
+player1.name = "lawrence"
+puts player1
+
+# Print player scores
