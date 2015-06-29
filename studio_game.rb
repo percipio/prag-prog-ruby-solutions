2f6ba8afb77966a98d6d51c647f01406d931b5bd
Joe Hale <jmartyhale@gmail.com>
2015-06-28 19:15:28 -0500

    Add Objects Interacting solutions

--- studio_game.rb
+++ studio_game.rb
@@ -31,70 +31,63 @@ class Player
 
 end
 
-# Instantiate 4 players
+class Game
+  attr_reader :title
+
+  def initialize(title)
+    @title = title.capitalize
+    @players = []
+  end
+
+  def add_player(player)
+    @players << player
+  end
+
+  def play
+
+    puts "New Game! #{@title} is on!"
+    puts "There are #{@players.size} players in #{@title}.\n\n"
+
+    @players.each do |player|
+      puts player
+      puts "\n"
+    end
+
+    @players.each do |player|
+      player.w00t
+      player.blam
+      puts player
+      puts "\n"
+    end
+
+  end
+end
+
+# Bonus round!
+# Instantiate 3 players
 player1 = Player.new("larry", 90)
 player2 = Player.new("curly", 120)
 player3 = Player.new("moe", 75)
 
-# Print out player's details
-puts player1
-puts player2
-puts player3
-puts "\n"
+# Create a game
+knuckleheads = Game.new("knuckleheads")
+# Add some players
+knuckleheads.add_player(player1)
+knuckleheads.add_player(player2)
+knuckleheads.add_player(player3)
+# Play the game already!
+knuckleheads.play
 
-# Blam or W00t players
-player1.w00t
-player2.blam
-player3.w00t
-puts "\n"
+# Instantiate 3 players
+player1 = Player.new("alvin", 120)
+player2 = Player.new("simon")
+player3 = Player.new("theodore", 85)
 
-# Print out player's new details
-puts player1
-puts player2
-puts player3
-puts "\n"
-
-# Print player's anonymous health
-puts player1.health
-puts player2.health
-puts player3.health
-puts "\n"
-
-# Change Larry's name to Lawrence, then print it.
-player1.name = "lawrence"
-puts player1
-
-# Create a array of players
-players = [player1, player2, player3]
-puts "\nThere are #{players.size} players in the game.\n\n"
-players.each do |player|
-  puts player
-end
-
-# Print out the player's health
-players.each do |player|
-  puts player .health
-end
-
-# Iterate through the players, w00t and blam them, then print it out.
-players.each do |player|
-  player.w00t
-  player.blam
-  puts player
-  puts "\n"
-end
-
-# Moe's not playing anymore, delete him
-players.pop
-
-puts players
-
-# Add another player to replace player3
-player3 = Player.new("Shemp", 125)
-players.push(player3)
-
-# Add a new line
-puts "\n"
-
-# Print the players again.
-puts players
+# Create a new game
+chipmunks = Game.new("chipmunks")
+# Add some players
+chipmunks.add_player(player1)
+chipmunks.add_player(player2)
+chipmunks.add_player(player3)
+# Play the new game
+chipmunks.play
